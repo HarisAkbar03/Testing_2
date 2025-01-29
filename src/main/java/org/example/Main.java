d600ebb564d35be9ae75560a37dd856e538d1b83
HarisAkbar03 <ha.akbar03@gmail.com>
2025-01-29 10:07:51 -0500

    completed till class car

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
 package org.example;
 
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!") ;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1 ; j <= i ; j++)
+            {
+                System.out.println("* ");
+            }
+            System.out.println();
         }
+    }
+    public class Vehicle
+    {
+        int numofwheels;
+        String color;
+        float enginesize;
+        String fueltype;
+
+        Vehicle()
+        {
+            numofwheels =4;
+            color = "black";
+            enginesize = 2.5F;
+            fueltype = "Gas";
+
+        }
+
+        float setenginesize(float esize)
+        {
+            enginesize = esize;
+            return esize;
+        }
+        String setcolor(String scolor)
+        {
+            color = scolor;
+            return color;
+        }
+        String setfuel(String ffuel)
+        {
+            fueltype = ffuel;
+            return fueltype;
+        }
+        int setnumofwheels(int wheels)
+        {
+            numofwheels = wheels;
+            return numofwheels;
+        }
+    }
+    public class Car extends Vehicle
+    {
+        String brand;
     }
 }
